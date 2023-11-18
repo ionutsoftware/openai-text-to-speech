f24dde64e5dd787fbaaf4ecacaa5a7cee79b26b3
ionut Sebastian Florea <tiflosebi@gmail.com>
2023-11-18 21:55:46 +0100

    create the repositori, adding  the *.py* file and creating the readme.md

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# openai-text-to-speech
-a text-to-speech program using the openai voices
+# OpenAI Text-to-Speech Program
+
+## Description:
+The OpenAI Text-to-Speech program is a simple console application utilizing the OpenAI Text-to-Speech API. It efficiently converts text into speech, saving the resulting audio file in MP3 format. The program provides a straightforward interface for users to input text, choose a voice, and select a model for speech generation.
+
+## Version History:
+- **Version 1.0:** Program Inception.
+
+## Changelog:
+- **Version 1.0:**
+  - Program creation, incorporating the OpenAI Text-to-Speech API to generate speech from text and save it as an MP3 file.
+
+## Bug Tracking:
+No known bugs in the current version.
+
+## Acknowledgments:
+A special thanks to the individuals contributing to the development of this program.
+
+Feel free to contribute and enhance this program!
---
+++ text_to_speech.py
@@ -0,0 +1,55 @@
+try:
+    from pathlib import Path
+    from openai import OpenAI
+    import json
+except ImportError as e:
+    print(f"Error importing modules. Error details: {str(e)}")
+    exit(1)
+
+def load_api_key(api_key_file):
+    try:
+        with open(api_key_file, "r") as json_file:
+            data = json.load(json_file)
+            return data.get("key")
+    except FileNotFoundError:
+        return None
+    except json.JSONDecodeError as e:
+        print(f"Error decoding the JSON file. {str(e)}")
+        return None
+
+def save_api_key(api_key_file, api_key):
+    key = {"key": api_key}
+    with open(api_key_file, "w") as json_file:
+        json.dump(key, json_file, indent=2)
+
+def text_to_speech(filename, model, voice, text, api_key):
+    try:
+        client = OpenAI(api_key=api_key)
+        file_path = Path(__file__).parent / f"{filename}.mp3"
+        
+        response = client.audio.speech.create(
+            model=model,
+            voice=voice,
+            input=text
+        )
+
+        response.stream_to_file(file_path)
+        print("Audio file created")
+
+    except error.ApiError as e:
+        print(f"API Error: {str(e)}")
+
+api_key_file = 'key.json'
+api_key = load_api_key(api_key_file)
+
+if api_key is None:
+    api_key = input("Enter your API key: ")
+    save_api_key(api_key_file, api_key)
+    print("API key registered in the file.")
+
+filename = input("Enter a name for your audio file (without extension): ")
+model = input('Enter a model, for example "tts-1": ')
+voice = input('Enter a voice, for example "alloy": ')
+text = input("Enter the text you want the voice to say: ")
+
+text_to_speech(filename, model, voice, text, api_key)
