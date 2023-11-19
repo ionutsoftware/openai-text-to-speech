cf71a492cf2278803ef0eb91a7e9040d64377b98
ionut Sebastian Florea <tiflosebi@gmail.com>
2023-11-19 13:13:10 +0100

    added a gui, and created a first release

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,10 +1,14 @@
 try:
     from pathlib import Path
     from openai import OpenAI
+    import wx
     import json
 except ImportError as e:
-    print(f"Error importing modules. Error details: {str(e)}")
+    show_info(None, f"Error importing modules. Error details: {str(e)}")
     exit(1)
+
+app = wx.App()
+app.MainLoop()
 
 def load_api_key(api_key_file):
     try:
@@ -14,13 +18,57 @@ def load_api_key(api_key_file):
     except FileNotFoundError:
         return None
     except json.JSONDecodeError as e:
-        print(f"Error decoding the JSON file. {str(e)}")
+        show_info(None, f"Error decoding the JSON file. {str(e)}")
         return None
 
 def save_api_key(api_key_file, api_key):
     key = {"key": api_key}
     with open(api_key_file, "w") as json_file:
         json.dump(key, json_file, indent=2)
+
+def ask(parent=None, message='', default_value='', multiline=False):
+    result = default_value
+
+    try:
+        if multiline:
+            dlg = wx.Dialog(parent, title=message)
+            sizer = wx.BoxSizer(wx.VERTICAL)
+            text_ctrl = wx.TextCtrl(dlg, style=wx.TE_MULTILINE | wx.TE_PROCESS_ENTER, value=default_value)
+            sizer.Add(text_ctrl, 1, wx.EXPAND | wx.ALL, 10)
+
+            btn_ok = wx.Button(dlg, wx.ID_OK, label="OK")
+            btn_cancel = wx.Button(dlg, wx.ID_CANCEL, label="Cancel")
+            btn_sizer = wx.StdDialogButtonSizer()
+            btn_sizer.AddButton(btn_ok)
+            btn_sizer.AddButton(btn_cancel)
+            btn_sizer.Realize()
+
+            sizer.Add(btn_sizer, 0, wx.ALL | wx.ALIGN_RIGHT, 10)
+            dlg.SetSizer(sizer)
+
+            if dlg.ShowModal() == wx.ID_OK:
+                result = text_ctrl.GetValue()
+
+        else:
+            dlg = wx.TextEntryDialog(parent, message, value=default_value)
+
+            if dlg.ShowModal() == wx.ID_OK:
+                result = dlg.GetValue()
+
+    finally:
+        dlg.Destroy()
+
+    return result
+
+def show_info(parent, message):
+    try:
+        dlg = wx.MessageDialog(parent, message, "Information", wx.OK | wx.ICON_INFORMATION)
+        dlg.ShowModal()
+    except wx.PyNoAppError:
+        pass
+    finally:
+        dlg.Destroy()
+
 
 def text_to_speech(filename, model, voice, text, api_key):
     try:
@@ -43,16 +91,16 @@ api_key_file = 'key.json'
 api_key = load_api_key(api_key_file)
 
 if api_key is None:
-    api_key = input("Enter your API key: ")
+    api_key = ask(parent=None, message="Enter your API key:", default_value="", multiline=False)
     save_api_key(api_key_file, api_key)
-    print("API key registered in the file.")
-try:
-    filename = input("Enter a name for your audio file (without extension): ")
-    model = input('Enter a model, for example "tts-1": ')
-    voice = input('Enter a voice, for example "alloy": ')
-    text = input("Enter the text you want the voice to say: ")
+    show_info(None, "The API key has been registered in the key.json file")
 
-    text_to_speech(filename, model, voice, text, api_key)
+filename = ask(parent=None, message="Enter a name for your audio file (without extension):", default_value="", multiline=False)
+model = ask(parent=None, message='Enter a model to use, for example "tts-1":', default_value="tts-1", multiline=False)
+voice = ask(parent=None, message='Enter a voice to use for converting your text. By default, the voice is "alloy":', default_value="alloy", multiline=False)
+text = ask(parent=None, message="Enter the text you want the voice to say:", default_value="", multiline=True)
 
-except KeyboardInterrupt:
-    print("\n process canceled by the user")
+text_to_speech(filename, model, voice, text, api_key)
+
+show_info(None, "The file has been correctly created")
+
